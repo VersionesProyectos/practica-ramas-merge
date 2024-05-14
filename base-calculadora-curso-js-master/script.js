29897236da6c715879a7e80bb82b743cc23b068b
Gerardo <versionesg@gmail.com>
2024-05-14 10:45:17 -0500

    se coloca nuevo boton de borrar

--- base-calculadora-curso-js-master/script.js
+++ base-calculadora-curso-js-master/script.js
@@ -11,3 +11,8 @@ function calcular() {
    const resultado = eval(valorPantalla)
    document.getElementById('pantalla').value = resultado 
 }
+
+function deshacer() {
+    const valorPantalla = document.getElementById('pantalla').value
+    document.getElementById('pantalla').value = valorPantalla.slice(0,-1)
+}
